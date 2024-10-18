b5eb672a5eab5a7b33f2a487aba4a4d946210dbd
RizkiMaula <Rmaulana12321@gmail.com>
2024-10-18 11:18:06 +0700

    coba buat nav baru di admin

--- src/components/fragmentes/NavbarAdmin.jsx
+++ src/components/fragmentes/NavbarAdmin.jsx
@@ -6,6 +6,7 @@ import axios from 'axios';
 import { Bars3Icon } from '@heroicons/react/24/outline';
 import NavListLib from './NavList';
 import { useState } from 'react';
+import { NavList } from '../elements/MenuList';
 
 const NavbarAdmin = () => {
   const [token, setToken] = useLocalStorage('authToken', '');
